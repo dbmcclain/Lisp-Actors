c77236f7e53dfe5d7f06a439db28a4d59b22d86e
David McClain <dbm@refined-audiometrics.com>
2020-11-15 18:05:49 -0700

    Update dynamic-wind.lisp
    rename items to make it simpler to explain how this all works

--- useful-macros/dynamic-wind.lisp
+++ useful-macros/dynamic-wind.lisp
@@ -8,38 +8,36 @@
 (defvar *dynamic-wind-stack* nil)
 
 (defstruct (dynamic-environment
-            (:constructor make-dynamic-environment (dynamic-winds)))
-  (dynamic-winds nil :read-only t))
+            (:constructor make-dynamic-environment (wrapper-fns)))
+  (wrapper-fns nil :read-only t))
 
 (defun capture-dynamic-environment ()
   (make-dynamic-environment (reverse *dynamic-wind-stack*)))
 
 (defmacro dynamic-wind (&body body)
-  (lw:with-unique-names (wind-fn thunk proceed-body)
-    `(flet ((,wind-fn (,thunk)
+  (lw:with-unique-names (wrapper-fn thunk proceed-body)
+    `(flet ((,wrapper-fn (,thunk)
               (macrolet ((proceed (&body ,proceed-body)
                            `(if ,',thunk
                                 (funcall ,',thunk)
                               (progn
                                 ,@,proceed-body))))
                 ,@body)))
-       (let ((*dynamic-wind-stack* (cons #',wind-fn *dynamic-wind-stack*)))
-         (,wind-fn nil)))
+       (let ((*dynamic-wind-stack* (cons #',wrapper-fn *dynamic-wind-stack*)))
+         (,wrapper-fn nil)))
     ))
 
 (defun call-with-dynamic-environment (env thunk)
-  (labels ((instantiate (env)
-             (cond (env
-                    (let* ((wind-fn (first env))
-                           (*dynamic-wind-stack* (cons wind-fn *dynamic-wind-stack*)))
-                      (funcall wind-fn (lambda ()
-                                         (instantiate (rest env))))
-                      ))
-                   (t
-                    (funcall thunk))
-                   )))
-    (with-slots (dynamic-winds) env
-      (instantiate dynamic-winds))))
+  (labels ((wrap-env (env)
+             (if env
+                 (let* ((wrapper-fn (first env))
+                        (*dynamic-wind-stack* (cons wrapper-fn *dynamic-wind-stack*)))
+                   (funcall wrapper-fn (lambda ()
+                                         (wrap-env (rest env)))))
+               ;; else
+               (funcall thunk))))
+    (with-slots (wrapper-fns) env
+      (wrap-env wrapper-fns))))
 
 (defmacro with-dynamic-environment ((env) &body body)
   `(call-with-dynamic-environment ,env (lambda ()
