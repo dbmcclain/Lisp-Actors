b9cc04864eecc61bdd27579a8e05b136ee387b64
David McClain <dbm@refined-audiometrics.com>
2021-11-27 18:33:33 -0700

    Update singletons.lisp

--- xTActors/singletons.lisp
+++ xTActors/singletons.lisp
@@ -7,14 +7,17 @@
 
 (defmacro def-singleton-actor (name args &body body)
   ;; useful for defining Actors that behave as a global list
-  (lw:with-unique-names (actor)
+  (lw:with-unique-names (actor tmp-actor)
     `(progn
        (defvar ,actor  nil)
        (defun ,name ,args
          (or ,actor
-             (setf ,actor (progn
-                            (pushnew ',actor *singleton-actors*)
-                            ,@body)))))
+             (let ((,tmp-actor (progn
+                                 ,@body)))
+               (assert (actor-p ,tmp-actor))
+               (pushnew ',actor *singleton-actors*)
+               (setf ,actor ,tmp-actor))
+             )))
     ))
 
 (defun reset-singleton-actors ()
