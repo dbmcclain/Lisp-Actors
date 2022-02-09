68f3af110a21511a53a3104a6caa89ee277eec1c
David McClain <dbm@refined-audiometrics.com>
2022-02-09 04:42:14 -0700

    Update sync-msg.lisp

--- xTActors/sync-msg.lisp
+++ xTActors/sync-msg.lisp
@@ -25,8 +25,8 @@
 ;; Customer Actors should be prepared to handle rendezvous failures in
 ;; addition to normal traffic - at both ends, sender and receiver.
 ;;
-;; Events (here Actors) are mappings A: f(E) -> E, with composition
-;; defined by, f • g = f(g(E)).
+;; Events, E, (here Actors) are mappings f: f(E) -> E, with
+;; composition defined by, f • g = f(g(E)).
 
 (in-package :ac)
 
