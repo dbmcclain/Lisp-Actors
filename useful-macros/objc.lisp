a796ea3da570b81d0f830489f93901cd9fefb623
David McClain <dbm@refined-audiometrics.com>
2022-09-18 10:52:32 -0700

    Create objc.lisp

---
+++ useful-macros/objc.lisp
@@ -0,0 +1,28 @@
+
+(in-package :um)
+
+;; ------------------------------------------------------------
+;; DM/RAL 09/22 - Try to make OBJC:INVOKE use Mac-like arguments.
+
+(defun st-to-objc (&rest args)
+  (let* ((grpd  (group args 2))
+         (names (mapcar #'first grpd))
+         (vals  (mapcar #'second grpd)))
+    (cons (apply #'concatenate 'string names) vals)))
+
+#|
+(objc-invoke-st "initWithBitmapDataPlanes:" nil
+            "pixelsWide:"      1
+            "pixelsHigh:"      1
+            "bitsPerSample:"   8
+            "samplesPerPixel:" 4
+            "hasAlpha:"        T
+            "isPlanar:"        NIL
+            "colorSpaceName:"  (ns-device-rgb-color-space)
+            "bytesPerRow:"     4
+            "bitsPerPixel:"    32)
+ |#
+
+(defmacro objc-invoke-st (&rest args)
+  ;; i.e., using Smalltalk syntax
+  `(objc:invoke ,@(apply #'st-to-objc args)))
