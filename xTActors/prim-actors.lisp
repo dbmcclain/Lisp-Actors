469019f9ff86603833962228066c585bbd70d372
David McClain <dbm@refined-audiometrics.com>
2022-10-15 11:05:12 -0700

    cruft removal

--- xTActors/prim-actors.lisp
+++ xTActors/prim-actors.lisp
@@ -10,8 +10,9 @@
 ;;  1. When an Actor expects a customer argument, it is always in
 ;;  first position.
 ;;
-;;  2. When an Actor uses DCASE, it expects the dispatch token in
-;;  second position when a customer arg is present.
+;;  2. Sink Actors don't have customers, unless they live behind a
+;;  SERIALIZER gate. Every Actor living behind a SERIALIZER *must*
+;;  eventually send a response to the customer.
 ;;
 ;; -------------------------------------------------------
 
@@ -163,57 +164,8 @@
             (send-combined-msg cust msg-hd msg-tl)))
         ))))
 
-;; -----------------------------------
-;; PAR - make an Actor that evaluates a series of blocks concurrently.
-;; Each block is fed the same initial message, and the results from
-;; each block are sent as an ordered collection to cust.
-#|
-(defun join-beh (cust lbl1)
-  ;; Join a pair of two possible messages into one response. One of the
-  ;; incoming messages will be labeled lbl1, while the other has
-  ;; another label. There are only two possible incoming incoming
-  ;; messages, because in use, our Actor is ephemeral and anonymous. So no
-  ;; other incoming messages are possible.
-  (lambda (lbl &rest msg)
-    (cond ((eq lbl lbl1)
-           (become (lambda (_ &rest msg2)
-                     (declare (ignore _))
-                     (send-combined-msg cust msg msg2))
-                   ))
-          (t ;; could only be lbl2
-             (become (lambda (_ &rest msg1)
-                       (declare (ignore _))
-                       (send-combined-msg cust msg1 msg))
-                     ))
-          )))
-
-(defun fork (left right)
-  ;; Accept two message lists, lreq and rreq, sending lreq to left,
-  ;; and rreq to right, collecting combined results into one ordered
-  ;; response.
-  (actor (cust lreq rreq)
-    (actors ((join  (create (join-beh cust tag-l)))
-             (tag-l (tag join))
-             (tag-r (tag join)))
-      (send* left tag-l lreq)
-      (send* right tag-r rreq))
-    ))
-
-(deflex par
-  ;; Send same msg to all actors in the lst, running them
-  ;; concurrently, and collect the results into one ordered response.
-  (α (cust lst &rest msg)
-    (if (null lst)
-        (send cust)
-      (actors ((join    (create (join-beh cust tag-car)))
-               (tag-car (tag join))
-               (tag-cdr (tag join)))
-        (send* (car lst) tag-car msg)
-        (send* self tag-cdr (cdr lst) msg)))
-    ))
-|#
 ;; -----------------------------------------------------------
-;; The above FORK/JOIN ran into MULTIPLE-VALUES-LIMIT in a real
+;; The previous FORK/JOIN ran into MULTIPLE-VALUES-LIMIT in a real
 ;; application.
 ;;
 ;; So the alternative must have participant Actors accepting a
@@ -311,30 +263,7 @@
   (send fut println))
  |#
 ;; -----------------------------------------
-;; Delayed Trigger
-
-#|
-(defun scheduled-message-beh (actor dt &rest msg)
-  ;; This is an interesting case... recall that *SEND* is dynamically
-  ;; bound, and its binding varies between foreign threads, and the
-  ;; various Actor dispatch engines.
-  ;;
-  ;; But timer events are like interrupt routines, and can happen on
-  ;; any thread and at any time. The only reasonable alternative here
-  ;; is to send the message to the community mailbox and let the
-  ;; dispatch routines take it from there.
-  ;;
-  ;; What to do about the single-thread dispatcher?
-  ;;
-  (if (actor-p actor)
-    (let ((timer (apply #'mp:make-timer #'send-to-pool actor msg)))
-      (lambda* _
-        (mp:schedule-timer-relative timer dt)))
-    #'lw:do-nothing))
-
-(defun scheduled-message (actor dt &rest msg)
-  (create (apply #'scheduled-message-beh actor dt msg)))
-|#
+;; Delayed Send
 
 (defun send-after (dt actor &rest msg)
   ;; NOTE: Actors, except those at the edge, must never do anything
@@ -352,143 +281,20 @@
   ;; we are the only ones that know about this edge Actor.
   ;;
   (when (actor-p actor)
-    (let ((timer (apply #'mpc:make-timer #'send-to-pool actor msg)))
+    (let ((timer (mpc:make-timer (lambda ()
+                                   ;; If we have a binding for SELF,
+                                   ;; then SEND is also redirected. We
+                                   ;; need to avoid using that
+                                   ;; version... But it also means
+                                   ;; that someone is listening to the
+                                   ;; Central Mailbox.
+                                   (let ((fn (if self #'send-to-pool #'send)))
+                                     (apply fn actor msg)))
+                                 )))
       (send (α ()
               (mpc:schedule-timer-relative timer dt)))
       )))
 
-;; -----------------------------------------
-;; Serializer Gateway
-;;
-;; This kind of Actor widget is not needed in our 1-Core-per-Actor
-;; system. Every Actor already has a message queue that serializes
-;; requests for service.
-;;
-;; It would be useful in a single-threaded implementation which must
-;; continue to dispatch messages to remain lively.
-;;
-;; We default to shared par-safe behavior because SERIALIZERs are
-;; frequently used for shared access to a resource. And since we use
-;; BECOME, we have to make the SERIALIZER have par-safe behavior.
-;;
-;; As with PAR-SAFE and IO, any cust args should be fully specified
-;; sponsored-actors.
-
-#|
-  ;; This version takes advantage of the already existing event queue
-  ;; in the sponsor. However, it also causes the CPU to spin
-  ;; needlessly.
-(defun serializer-beh (service)
-  ;; initial empty state
-  (lambda (cust &rest msg)
-    (let ((tag  (tag self)))
-      (send* service tag msg)
-      (become (enqueued-serializer-beh
-               service tag cust))
-      )))
-
-(defun enqueued-serializer-beh (service tag in-cust)
-  (lambda (cust &rest msg)
-    (cond ((eq cust tag)
-           (send* in-cust msg)
-           (become (serializer-beh service)))
-          
-          (t
-           (repeat-send self))
-          )))
-|#
-
-#|
-;; This version does not cause the CPU to spin
-(defun serializer-beh (service)
-   ;; initial non-busy state
-   (alambda
-    ((cust . msg)
-     (let ((tag  (tag self)))
-       (send* service tag msg)
-       (become (busy-serializer-beh
-                service tag cust +emptyq+))
-       ))))
-
-(defun busy-serializer-beh (service tag in-cust queue)
-  (alambda
-   ((atag . ans) when (eql atag tag)
-    (send* in-cust ans)
-    (if (emptyq? queue)
-        (become (serializer-beh service))
-      (multiple-value-bind (next-req new-queue) (popq queue)
-        (destructuring-bind (next-cust . next-msg) next-req
-          (let ((new-tag (tag self)))
-            (send* service new-tag next-msg)
-            (become (busy-serializer-beh
-                     service new-tag next-cust new-queue))
-            )))
-      ))
-
-   (msg
-    (become (busy-serializer-beh
-             service tag in-cust
-             (addq queue msg))
-            ))
-   ))
-
-(defun serializer (service)
-  (create (serializer-beh service)))
-
-(defun serializer-sink (service)
-  ;; Turn a service into a sink. Service must accept a cust argument,
-  ;; and always send a response to cust.
-  (label (serializer service) sink))
-
-|#
-;; ----------------------------------------------------
-;; Safe Serializer - serializer with unblocking channel and timeout
-#|
-(defun new-pend-serializer-beh (svc ret timeout cust waitq msg)
-  (actors ((gate  (once ret))
-           (tmout (tag gate))
-           (reply (tag gate)))
-    (send-after timeout tmout)
-    (become (pend-serializer-beh svc ret timeout cust tmout reply waitq))
-    (send* svc reply msg)))
-
-(defun no-pend-serializer-beh (svc ret timeout)
-  (alambda
-   ((tag . _) / (eql tag ret))
-
-   ((cust . msg)
-    (new-pend-serializer-beh svc ret timeout cust +emptyq+ msg))
-   ))
-
-(defun pend-serializer-beh (svc ret timeout cur-cust tmout reply waitq)
-  (alambda
-   ((tag type-tag . ans) / (eql tag ret)
-    (when (or (and (eql type-tag reply)
-                   (or (send* cur-cust ans)
-                       t))
-              (eql type-tag tmout)
-              (eql type-tag self))
-      (if (emptyq? waitq)
-          (become (no-pend-serializer-beh svc ret timeout))
-        (multiple-value-bind (top newq) (popq waitq)
-          (destructuring-bind (new-cust . new-msg) top
-            (new-pend-serializer-beh svc ret timeout new-cust newq new-msg)
-            ))
-        )))
-
-   (msg
-    (become (pend-serializer-beh svc ret timeout cur-cust tmout reply (addq waitq msg))))
-   ))
-
-(defun serializer (svc &optional (timeout 10))
-  (actors ((ret  (tag gate))
-           (gate (create (no-pend-serializer-beh svc ret timeout))))
-    (values gate ret)))
-
-(defun unblock-serializer (ser ret)
-  ;; emergency use back door
-  (send ret ser))
-|#
 ;; -----------------------------------------
 ;; Serializer Gateway - service must always respond to a customer
 ;;
@@ -559,7 +365,7 @@
     (let* ((dst (create (doit-beh)))
            (x (serializer
                (α (cust)
-                 (sleep 0.1)
+                 (sleep 0.19999)
                  (send cust :ok))
                :timeout 0.2)
               ))
@@ -589,66 +395,6 @@
   (send timer println 1))
 |#
 
-;; -----------------------------------------------
-;; For sequenced message delivery
-;;
-;; When a messages arrives out of order, send it with :WAIT to the
-;; pending items list. When you are ready for any particular sequence
-;; number or label, then send :READY with that seequence number to the
-;; pending list. If it had previously arrived, it will be re-sent.
-;;
-;; The purpose of this Actor is to avoid spinning on messages,
-;; needlessly using CPU cycles.
-#|
-;; PRUNED-BEH suffers from race conditions in a parallel concurrent
-;; Actors system...  Proper behavior in the face of parallel execution
-;; requires that all visible state changes occur behind one BECOME.
-;; Prunable lists fail in this regard, distributing the excision among
-;; several messages.
-
-(def-beh pruned-beh (next cust)
-  ((:pruned beh)
-   (send cust :ok)
-   (become beh))
-  
-  (msg
-   (send* next msg)))
-
-(defmacro prunable-alambda (&rest clauses)
-  (lw:with-unique-names (tmp-next tmp-cust)
-    `(macrolet ((prune-self (next &optional cust)
-                  `(let ((,',tmp-next ,next)
-                         (,',tmp-cust ,cust))
-                     (become (pruned-beh ,',tmp-next ,',tmp-cust))
-                     (send ,',tmp-next self :prune))
-                  ))
-       (alambda 
-        ((cust :prune)
-         (send cust :pruned self-beh))
-        ,@clauses)
-       )))
-
-(defun no-pend-beh ()
-  (prunable-alambda
-    ((:wait ctr . msg)
-     (let ((next (create
-                  (no-pend-beh))))
-       (become (pend-beh ctr msg next))))
-    ))
-
-(defun pend-beh (ctr msg next)
-  (prunable-alambda
-    ((cust :ready in-ctr) when (eql ctr in-ctr)
-     (send* cust ctr msg)
-     (prune-self next))
-    
-    (msg
-     (send* next msg))
-    ))
-    
-(defun sequenced-delivery ()
-  (create (no-pend-beh)))
-|#
 ;; ---------------------------------------------------------
 ;; SEQUENCED-DELIVERY
 ;;   Provider sends :DELIVER with sequence counter and message
@@ -679,44 +425,6 @@
 (defun sequenced-delivery ()
   (create (sequenced-beh)))
 
-;; --------------------------------------------------
-#|
-(def-beh suspended-beh (prev-beh tag queue)
-  ((atag) when (eq tag atag)
-   (become prev-beh)
-   (do-queue (item queue)
-     (send* self item)))
-  
-  (msg
-   (become (suspended-beh prev-beh tag (addq queue msg)))))
-   
-(defun suspend ()
-  ;; To be used only inside of Actor behavior code.
-  ;; Just send to the tag to resume the Actor.
-  ;; (Watch out for cross-thread actions...)
-  (let ((tag (tag self)))
-    (become (suspended-beh self-beh tag +emptyq+))
-    tag))
-|#
-#|
-;; Example of using SUSPENDED-BEH to serialize host Actor with
-;; embedded Beta forms:
-
-  ... ;; inside host Actor
-  (let ((resume (suspend)))
-    (beta (ans)
-        (send some-actor beta msg))
-      .... beta body forms...
-      (send resume)
-      ))
-
-;; Afer SUSPEND, instead of the beta form operating concurrently with
-;; the enclosing host Actor, the host Actor suspends its normal
-;; message handling, enqueueing all arriving messages except those
-;; that arrive via the tag. Once the send to the resume tag happens,
-;; the host Actor resumes its prior behavior, and handles all the
-;; enqueued messages.
-|#
 ;; -------------------------------------
 ;; Systolic Processing Pipelines
 
@@ -766,49 +474,6 @@
 (defun tee (&optional sink-blk)
   (create (tee-beh sink-blk)))
 
-;; ---------------------------------------------------------
-#|
-(defun ticketed-perform-beh ()
-  (alambda
-   ((cust :req)
-    (let ((tag  (tag self)))
-      (become (pending-perform-beh tag +emptyq+))
-      (send cust tag)
-      (send-after 1 tag :done)
-      ))
-   ))
-
-(defun pending-perform-beh (tag pend)
-  (alambda
-   ((cust :done) when (eq cust tag)
-    (if (emptyq? pend)
-        (become (ticketed-perform-beh))
-      (multiple-value-bind (next-cust new-queue) (popq pend)
-        (let ((new-tag (tag self)))
-          (send next-cust new-tag)
-          (send-after 1 new-tag :done)
-          (become (pending-perform-beh new-tag new-queue)))
-        )))
-   
-   ((cust :req)
-    (become (pending-perform-beh tag (addq pend cust))))
-   ))
-
-(defun ticketed-perform ()
-  (create (ticketed-perform-beh)))
-
-(defmacro with-ticket (ticket-master &body body)
-  (lw:with-unique-names (tag)
-    `(beta (,tag)
-         (send ,ticket-master self :req)
-       ,@body
-       (send ,tag :done))
-    ))
-
-#+:LISPWORKS
-(editor:setup-indent "with-ticket" 1)
-|#
-
 ;; ------------------------------------------------
 
 (defun with-watchdog-timer (timeout action on-timeout)
@@ -828,7 +493,7 @@
       )))
 
 #|
-(send (with-timeout 2.1 (actor (cust)
+(send (with-watchdog-timer 2.1 (actor (cust)
                         (send-after 2 cust :ok))
                     (actor _
                       (send println :nah)))
