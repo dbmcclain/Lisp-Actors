c8316bf2d664cc523ee7263687ed54597ec65c68
David McClain <dbm@refined-audiometrics.com>
2022-11-22 18:49:25 -0700

    UDP is not stream oriented. No need for self-sync encode/decode. Just 8-bit simple vectors.

--- xTActors/actors-extra/dm-udp.lisp
+++ xTActors/actors-extra/dm-udp.lisp
@@ -4,6 +4,8 @@
 ;; Camille Troillard. Extended to support typical client/server
 ;; architectures, and self-sync encoding, by DM/RAL 2022/11/21
 ;; 10:18:20.
+;; -- DM/RAL  2022/11/22 18:44:52 Remove Self-Sync encode/decode
+;;    Interface transmits and receives UB8 vectors.
 ;;
 ;; DM/RAL  2022/11/21 06:36:03
 ;; ----------------------------------
@@ -22,14 +24,9 @@
 ;; This UDP interface expects both Client and Server to send messages
 ;; that are 8-bit simple vectors. The "safe" size limit is around 512
 ;; bytes after self-sync encoding. This particular interace allows up
-;; to 1499 bytes total message size, which works out to 1485 before
-;; self-sync encoding. Apple Mac OSX sets an absolute max size of 9216
-;; bytes (9202 bytes before self-sync encoding). But the code here
-;; limits it to 1499 bytes.
+;; to 1499 bytes total message size. Apple Mac OSX sets an absolute
+;; max size of 9216 bytes. But the code here limits it to 1499 bytes.
 ;;
-;; So the asymmetry forces us to invent a modified Self-Sync decoder
-;; machine for use by the Server side. The Clients use the existing
-;; TCP/IP encoder machines.
 
 (defpackage #:dm-udp
   (:use #:common-lisp #:ac))
@@ -40,12 +37,6 @@
   (import '(vec-repr:ub8
             vec-repr:ub8-vector
             vec-repr:make-ub8-vector
-
-            self-synca:+long-count-base+
-            self-synca:+max-short-count+
-            self-synca:+max-long-count+
-            self-synca:vec-le4-to-int
-            self-synca:crc32
             )))
 
 ;; ----------------------------------
@@ -55,214 +46,12 @@
 
 (defparameter +udp-max-receive-size+ 1500)
 
-(defstruct user-cnx-state
-  actor
-  (in-counter 0))
-
-;; -----------------------------------------------------------------------
-;; Self-Sync Decoding for UDP Server Ports
-;;
-;; These have incoming message, serialized at the receiver, but also
-;; augmented with ip-addr and ip-port of sender. We need to pass that
-;; identifying information along to the ultimate handler.
-
-(defun udp-srv-ssfsm-beh (dest aout stuff-fn)
-  #F
-  (declare (function stuff-fn)
-           ((ub8-vector *) aout))
-  (let (state          ;; machine state function
-        need-fefd      ;; when T we may need to insert #xFEFD
-        (remct 0)      ;; segment bytes remaining
-        crcv           ;; copy of 4-byte CRC field
-        lenv           ;; copy of 4-byte length field
-        senders-ip     ;; sender's ip-addr
-        senders-port   ;; sender's ip-port
-        (nel   -1))    ;; expected message length
-    (declare (fixnum remct nel))
-    (macrolet ((new-state (fn)
-                 `(setf state #',fn)))
-      (labels (;; --------------------
-               ;; Utility Functions
-               (subrange-code? (b)
-                 (declare (ub8 b))
-                 (<= 0 b #xFC))
-               
-               (stuffer-init (ct)
-                 (declare (fixnum ct))
-                 (setf nel       -1
-                       remct     ct
-                       need-fefd (< ct +max-short-count+)
-                       (fill-pointer aout) 0)
-                 (new-state read-segm))
-
-               (segm-init (ct)
-                 (declare (fixnum ct))
-                 (setf remct     ct
-                       need-fefd (< ct +max-long-count+))
-                 (new-state read-segm)
-                 (check-finish))
-
-               (raw-stuff (b)
-                 (funcall stuff-fn b aout))
-               
-               (stuff (b)
-                 (raw-stuff b)
-                 (decf remct)
-                 (check-finish))
-
-               (check-finish ()
-                 (when (zerop remct)
-                   (when (minusp nel)
-                     (setf crcv (subseq aout 0 4)
-                           lenv (subseq aout 4 8)
-                           nel  (+ 8 (vec-le4-to-int lenv))))
-                   (let ((nbuf  (length aout)))
-                     (declare (fixnum nbuf))
-                     (when (and need-fefd
-                                (< nbuf nel))
-                       (raw-stuff #xFE)
-                       (raw-stuff #xFD)
-                       (incf nbuf 2))
-                     (cond ((< nbuf nel)
-                            (new-state read-long-count))
-                           (t
-                            (new-state start)
-                            (let* ((ans (subseq aout 8))
-                                   (chk (crc32 lenv ans)))
-                              (when (equalp crcv chk)
-                                (send dest senders-ip senders-port ans))
-                              ))
-                           ))))
-
-               (restart (b)
-                 (new-state start)
-                 (start b))
-
-               (inhale (b)
-                 (funcall state b))
-               
-               ;; ----------------
-               ;; Machine States
-               (start (b)
-                 (declare (ub8 b))
-                 (when (eql b #xFE)
-                   (new-state check-start-fd)))
-               
-               (check-start-fd (b)
-                 (declare (ub8 b))
-                 (if (eql b #xFD)
-                     (new-state check-version)
-                   (restart b)))
-
-               (check-version (b)
-                 (declare (ub8 b))
-                 (if (eql b #x01)
-                     (new-state read-short-count)
-                   (restart b)))
-
-               (read-short-count (b)
-                 (if (subrange-code? b)
-                     (stuffer-init b)
-                   (restart b)))
-
-               (read-segm (b)
-                 (declare (ub8 b))
-                 (if (and (eql b #xFE)
-                          (> remct 1))
-                     (new-state check-segm-fd)
-                   (stuff b)))
-
-               (check-segm-fd (b)
-                 (declare (ub8 b))
-                 (cond ((eql b #xFD) ;; we just saw a start pattern #xFE #xFD
-                        (new-state check-version))
-                       (t
-                        (stuff #xFE)
-                        (new-state read-segm)
-                        (read-segm b))
-                       ))
-
-               (read-long-count (b)
-                 (declare (ub8 b))
-                 (cond ((subrange-code? b)
-                        (setf remct b)
-                        (new-state read-long-count-2))
-                       (t
-                        (restart b))
-                       ))
-
-               (read-long-count-2 (b)
-                 (declare (ub8 b))
-                 (if (subrange-code? b)
-                     (segm-init (+ remct (* b +long-count-base+)))
-                   (restart b))))
-        
-        (new-state check-version) ;; initialize state
-        
-        ;; finally... we get to the Actor behavior function
-        (lambda (cust from-ip from-port buf)
-          (declare ((array ub8 *) buf))
-          (setf senders-ip   from-ip
-                senders-port from-port)
-          (map nil #'inhale buf)
-          (send cust :next))
-        ))))
-
-(defun udp-srv-decoder-fsm (dest)
-  (create (udp-srv-ssfsm-beh dest (make-ub8-vector 256
-                                                   :fill-pointer 0
-                                                   :adjustable   t)
-                             #'vector-push-extend)))
-
-;; ------------------------------------------------------
-;; Self Sync Stream Decoder for Server
-
-(defun udp-srv-stream-decoder-beh (fsm wait-ix queue)
-  (alambda
-   ((:deliver bufix from-ip from-port message)
-    (cond ((eql bufix wait-ix)
-           (send fsm self from-ip from-port message)
-           (become (udp-srv-busy-stream-decoder-beh fsm (1+ bufix) queue)))
-          
-          (t
-           (become (udp-srv-stream-decoder-beh
-                    fsm wait-ix
-                    (cons (list bufix from-ip from-port message) queue))))
-          ))
-   ))
-
-(defun udp-srv-busy-stream-decoder-beh (fsm wait-ix queue)
-  (alambda
-   ((:next)
-    (let ((lst (assoc wait-ix queue)))
-      (cond (lst
-             (destructuring-bind (bufix from-ip from-port message)
-                 lst
-               (declare (ignore bufix))
-               (send fsm self from-ip from-port message)
-               (become (udp-srv-busy-stream-decoder-beh fsm (1+ wait-ix) (remove lst queue)))
-               ))
-            (t
-             (become (udp-srv-stream-decoder-beh fsm wait-ix queue)))
-            )))
-
-   ((:deliver bufix from-ip from-port message)
-    (become (udp-srv-busy-stream-decoder-beh fsm wait-ix
-                                             (cons (list bufix from-ip from-port message) queue))))
-   ))
-
-(defun udp-srv-self-sync-stream-decoder (dest)
-  (let ((fsm (udp-srv-decoder-fsm dest)))
-    (create (udp-srv-stream-decoder-beh fsm 1 nil))))
-
 ;; -----------------------------------------------------------------------
 ;; Server Side using raw socket
 
 (defun udp-srv-port-process-message (io-state buffer number-of-bytes-read from-ip from-port)
-  (let ((cnx-state (comm:async-io-state-user-info io-state)))
-    (send (user-cnx-state-actor cnx-state)
-          :deliver
-          (incf (user-cnx-state-in-counter cnx-state))
+  (let ((actor (comm:async-io-state-user-info io-state)))
+    (send actor
           from-ip from-port
           (subseq buffer 0 number-of-bytes-read))
     (udp-srv-port-receive-next io-state buffer)))
@@ -278,7 +67,7 @@
                             (make-array +udp-max-receive-size+
                                         :element-type '(unsigned-byte 8))))
 
-(defun udp-srv-port-beh (io-state encoder)
+(defun udp-srv-port-beh (io-state)
   ;; Sending side for Server
   (alambda
    ((cust :dispose)
@@ -286,8 +75,7 @@
                                          :close-callback (λ _ (send cust :closed))))
    ((cust to-ip to-port message)
     (comm:async-io-state-send-message-to-address io-state
-                                                 to-ip to-port
-                                                 (funcall encoder message)
+                                                 to-ip to-port message
                                                  (λ _ (send cust :sent to-ip to-port message))))
    ))
 
@@ -300,21 +88,18 @@
                   :local-address local-address
                   :local-port local-port
                   :ipv6 ipv6
-                  :user-info (make-user-cnx-state
-                              :actor (udp-srv-self-sync-stream-decoder receive-actor)))))
+                  :user-info receive-actor)))
    (udp-srv-port-start-receive io-state)
-   (create (udp-srv-port-beh io-state #'self-synca:encode))))
+   (create (udp-srv-port-beh io-state))
+   ))
 
 ;; -----------------------------------------------------------------------
 ;; Client Side using connected socket - uses existing Self-Sync stream
 ;; interface.
 
 (defun udp-cli-port-process-message (io-state buffer number-of-bytes-read)
-  (let ((cnx-state (comm:async-io-state-user-info io-state)))
-    (send (user-cnx-state-actor cnx-state)
-          :deliver
-          (incf (user-cnx-state-in-counter cnx-state))
-          (subseq buffer 0 number-of-bytes-read))
+  (let ((actor (comm:async-io-state-user-info io-state)))
+    (send actor (subseq buffer 0 number-of-bytes-read))
    (udp-cli-port-receive-next io-state buffer)))
 
 (defun udp-cli-port-receive-next (io-state buffer)
@@ -327,21 +112,19 @@
                             (make-array +udp-max-receive-size+
                                         :element-type '(unsigned-byte 8))))
 
-(defun udp-cli-port-beh (io-state encoder)
+(defun udp-cli-port-beh (io-state)
   ;; Sending side for Client
  (alambda
   ((cust :dispose)
    (comm:async-io-state-abort-and-close io-state
                                         :close-callback (λ _ (send cust :closed))))
   ((cust message)
-   (comm:async-io-state-send-message io-state
-                                     (funcall encoder message)
+   (comm:async-io-state-send-message io-state message
                                      (λ _ (send cust :sent message))))
   ))
 
 (defun udp-cli-connected-port (hostspec service &key
                                    (ipv6 nil)
-                                   (encoder #'self-synca:encode)
                                    local-address local-port
                                    receive-actor)
  (let ((io-state (comm:create-async-io-state-and-connected-udp-socket
@@ -350,11 +133,11 @@
                   :local-address local-address
                   :local-port local-port
                   :ipv6 ipv6
-                  :user-info (make-user-cnx-state
-                              :actor (self-synca:stream-decoder receive-actor)))))
+                  :user-info receive-actor)))
    (when receive-actor
      (udp-cli-port-start-receive io-state))
-   (create (udp-cli-port-beh io-state encoder))))
+   (create (udp-cli-port-beh io-state))
+   ))
 
 ;; -------------------------------------------------------------
 #|
