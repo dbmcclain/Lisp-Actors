7a8dd111d1be7d3dc1a0c84bc5369af29f11fa8e
David McClain <dbm@refined-audiometrics.com>
2023-10-02 12:57:31 -0700

    For SBCL

--- useful-macros/zulu-time.lisp
+++ useful-macros/zulu-time.lisp
@@ -17,5 +17,16 @@
     (+ (get-universal-time)
        (* 3600 zone))))
 
+#+:LISPWORKS
 (defun zulu-date-string ()
   (format nil "~A UTC" (hcl:date-string (get-zulu-time))))
+
+#-:LISPWORKS
+(defun zulu-date-string ()
+  (multiple-value-bind (sec min hr date mon yr day daylight-p zone)
+      (decode-universal-time (get-zulu-time))
+    (declare (ignore daylight-p day zone))
+    (format nil "~{~2,'0D~^/~} ~{~2,'0D~^\:~} UTC"
+            (list (mod yr 100) mon date)
+            (list hr min sec))
+    ))
