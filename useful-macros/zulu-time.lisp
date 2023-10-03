3cb7e4b8287342b571e0994b1e068d9ee5f5ca11
David McClain <dbm@refined-audiometrics.com>
2023-10-03 04:12:59 -0700

    Update zulu-time.lisp

--- useful-macros/zulu-time.lisp
+++ useful-macros/zulu-time.lisp
@@ -12,23 +12,27 @@
 
 (defun get-zulu-time (&optional (universal-time (get-universal-time)))
   ;; The assumption here is that if you supply a time, it must have
-  ;; originated on this system.
+  ;; originated on this system, and it represents local time.
   (multiple-value-bind (sec min hr date mon yr day daylight-p zone)
-      (get-decoded-time)
-    (declare (ignore sec min hr date mon yr day daylight-p))
+      (decode-universal-time universal-time)
+    (declare (ignore sec min hr date mon yr day))
     (+ universal-time
-       (* 3600 zone))))
-
-#+:LISPWORKS
-(defun zulu-date-string (&optional (zulu-time (get-zulu-time)))
-  (format nil "~A UTC" (hcl:date-string zulu-time)))
-
-#-:LISPWORKS
-(defun zulu-date-string (&optional (zulu-time (get-zulu-time)))
-  (multiple-value-bind (sec min hr date mon yr day daylight-p zone)
-      (decode-universal-time zulu-time)
-    (declare (ignore daylight-p day zone))
-    (format nil "~{~2,'0D~^/~} ~{~2,'0D~^\:~} UTC"
-            (list yr mon date)
-            (list hr min sec))
+       (* 3600 (if daylight-p
+                   (1- zone)
+                 zone)))
     ))
+
+(defun zulu-date-string (&optional (zulu-time (get-universal-time)))
+  ;; we have to over-correct because decode thinks we are local time
+  (let ((local-time (get-zulu-time zulu-time)))
+    (multiple-value-bind (sec min hr date mon yr day daylight-p zone)
+        (decode-universal-time local-time)
+      (declare (ignore daylight-p day zone))
+      (format nil "~{~2,'0D~^/~} ~{~2,'0D~^\:~} UTC"
+              (list yr mon date)
+              (list hr min sec))
+      )))
+
+#|
+(zulu-date-string 0) => "1900/01/01 00:00:00 UTC" 
+ |#
