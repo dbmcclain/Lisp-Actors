2e5c9b0ec8c4abd785ca263f16cc2b01c8c59c83
David McClain <dbm@refined-audiometrics.com>
2025-09-23 11:02:14 -0700

    Update anti-collision.lisp

--- xTActors/actors-base/anti-collision.lisp
+++ xTActors/actors-base/anti-collision.lisp
@@ -30,53 +30,51 @@
 
 ;; --------------------------------------------
 
-(defvar *become-bak*  nil)
-
-(defun do-without-contention (guard clos)
+(defun make-without-contention-fn (guard become-hook)
   (declare (cons guard)
-           (function clos))
+           (function become-hook))
   (symbol-macrolet ((owner  (car (the cons guard))))
-    (let ((*become-hook* *become-bak*)
-          (me  (mpc:get-current-process)))
-          
-      (flet ((go-around ()
-               (%send-to-pool (msg self self-msg))
-               (abort))
-             (acquire ()
-               (mpc:compare-and-swap owner nil me))
-             (release (&rest ignored)
-               (declare (ignore ignored))
-               (mpc:compare-and-swap owner me nil)))
-        (declare (dynamic-extent #'go-around #'acquire))
-        ;;
-        ;; First thread to attempt WITHOUT-CONTENTION takes it,
-        ;; preemptively blocking all other threads from mutating the
-        ;; Actor. Other threads simply put their message back on the
-        ;; queue for later delivery.
-        ;;
-        ;; Non-mutating threads continue to execute in parallel
-        ;; concurrent manner. So it continues to be a good idea to
-        ;; keep the behavior code functionally pure.
-        ;;
-        (when (acquire)
-          (send (create #'release)))
-        (if (eq me owner)
-            (handler-bind
-                ((error #'release))
-              (funcall clos))
-          ;; else, Re-enqueue our message for later delivery,
-          ;; and go process the next available message. This
-          ;; drops all pending BECOME and SEND.
-          (go-around))
-        ))))
+    (lambda (clos)
+      (declare (function clos))
+      (let ((*become-hook* become-hook)
+            (me  (mpc:get-current-process)))
+        
+        (flet ((go-around ()
+                 (%send-to-pool (msg self self-msg))
+                 (abort))
+               (acquire ()
+                 (mpc:compare-and-swap owner nil me))
+               (release (&rest ignored)
+                 (declare (ignore ignored))
+                 (mpc:compare-and-swap owner me nil)))
+          (declare (dynamic-extent #'go-around #'acquire))
+          ;;
+          ;; First thread to attempt WITHOUT-CONTENTION takes it,
+          ;; preemptively blocking all other threads from mutating the
+          ;; Actor. Other threads simply put their message back on the
+          ;; queue for later delivery.
+          ;;
+          ;; Non-mutating threads continue to execute in parallel
+          ;; concurrent manner. So it continues to be a good idea to
+          ;; keep the behavior code functionally pure.
+          ;;
+          (when (acquire)
+            (send (create #'release)))
+          (if (eq me owner)
+              (handler-bind
+                  ((error #'release))
+                (funcall clos))
+            ;; else, Re-enqueue our message for later delivery,
+            ;; and go process the next available message. This
+            ;; drops all pending BECOME and SEND.
+            (go-around))
+          )))
+    ))
 
 (defun do-without-become (beh-fn msg)
-  (flet ((bad-become (&rest ignored)
-           (declare (ignore ignored))
-           (error "Unguarded BECOME in contention-free semantics")))
-    (let ((*become-bak*  *become-hook*)
-          (*become-hook* #'bad-become))
-      (apply beh-fn msg))))
+  (let ((*become-hook* (lambda* _
+                         (error "Unguarded BECOME in contention-free semantics"))))
+    (apply beh-fn msg)))
   
 (defmacro with-contention-free-semantics (beh-fn-form)
   ;; Should wrap a behavior function.
@@ -94,14 +92,15 @@
   ;; code. But you would miss the ones in external functions. We are
   ;; left with runtime error trapping.
   ;;
-  (um:with-unique-names (g!guard g!msg g!body)
+  (um:with-unique-names (g!guard g!msg g!body g!cf-free-fn)
     `(let ((,g!guard (list nil)))
        (lambda (&rest ,g!msg)
-         (do-without-become
-          (macrolet ((without-contention (&body ,g!body)
-                       `(do-without-contention ,',g!guard (lambda ()
-                                                            ,@,g!body))))
-            ,beh-fn-form)
-          ,g!msg)))
+         (let ((,g!cf-free-fn (make-without-contention-fn ,g!guard *become-hook*)))
+           (do-without-become
+            (macrolet ((without-contention (&body ,g!body)
+                         `(funcall ,',g!cf-free-fn (lambda ()
+                                                     ,@,g!body))))
+              ,beh-fn-form)
+            ,g!msg))))
     ))
 
