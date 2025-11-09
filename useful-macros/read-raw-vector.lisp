aa421c68106753deec3b87c5876cbab0b2f0c13a
David McClain <dbm@refined-audiometrics.com>
2025-11-09 03:17:36 -0700

    Update read-raw-vector.lisp

--- useful-macros/read-raw-vector.lisp
+++ useful-macros/read-raw-vector.lisp
@@ -50,26 +50,26 @@
              (buf                 (make-array (* csiz nr-elts-per-buf)
                                               :element-type '(unsigned-byte 8)))
              (end                 (or end (length sequence)))
-             (pos                 start)
-             (pel                 (fli:make-pointer
-                                   :type    ctyp
-                                   :address (+ 8 (sys:object-address buf)))
-                                  ))
+             (pos                 start))
         (declare (dynamic-extent buf)
                  (fixnum csiz end pos nr-elts-per-buf))
-        (while (< pos end)
-          (let* ((nr-bytes-to-read (* csiz (min nr-elts-per-buf (- end pos))))
-                 (nr-bytes-read    (read-sequence buf stream :end nr-bytes-to-read))
-                 (nr-elts-read     (truncate nr-bytes-read csiz)))
-            (declare (fixnum nr-bytes-to-read nr-bytes-read nr-elts-read))
-            (when (plusp nr-elts-read)
-              (fli:replace-foreign-array sequence pel
-                                         :start1 pos
-                                         :end2   nr-elts-read)
-              (incf pos nr-elts-read))
-            (when (< nr-bytes-read nr-bytes-to-read)
-              (setf end pos))
-            ))
-        pos ))
-    ))
+        (fli:with-dynamic-foreign-objects ((pb :uint8 :nelems (length buf)))
+          (fli:with-coerced-pointer (pel :type ctyp) pb
+            (while (< pos end)
+              (let* ((nr-bytes-to-read (* csiz (min nr-elts-per-buf (- end pos))))
+                     (nr-bytes-read    (read-sequence buf stream :end nr-bytes-to-read))
+                     (nr-elts-read     (truncate nr-bytes-read csiz)))
+                (declare (fixnum nr-bytes-to-read nr-bytes-read nr-elts-read))
+                (when (plusp nr-elts-read)
+                  (fli:replace-foreign-array pb       buf
+                                             :end1    nr-bytes-read)
+                  (fli:replace-foreign-array sequence pel
+                                             :start1 pos
+                                             :end2   nr-elts-read)
+                  (incf pos nr-elts-read))
+                (when (< nr-bytes-read nr-bytes-to-read)
+                  (setf end pos))
+                ))
+            pos ))
+        ))))
 
