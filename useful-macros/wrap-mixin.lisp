19fd9de335bbb1111e539e60d225c34b2fb7089b
David McClain <dbm@refined-audiometrics.com>
2025-11-10 03:23:16 -0700

    Update wrap-mixin.lisp

--- useful-macros/wrap-mixin.lisp
+++ useful-macros/wrap-mixin.lisp
@@ -11,36 +11,37 @@
 ;; ----------------------------------
 
 (defun wrap-instance-with-mixin (obj mixin-class &key name-prefix class-name)
-  (let* ((mixin-class  (if (symbolp mixin-class)
-                           (find-class mixin-class)
-                         mixin-class))
-         (obj-class    (class-of obj))
-         (class-name   (cond ((null class-name)
-                              (let ((obj-class-name (class-name obj-class)))
-                                (intern (concatenate 'string
-                                                     (or (and name-prefix
-                                                              (string name-prefix))
-                                                         (string (class-name mixin-class)))
-                                                     "-"
-                                                     (string (package-name (symbol-package obj-class-name)))
-                                                     "-"
-                                                     (string obj-class-name))
-                                        (symbol-package (class-name mixin-class)))
-                                ))
-                              
-                             ((symbolp class-name)
-                              class-name)
-
-                             (t
-                              (intern (string class-name)
-                                      (symbol-package (class-name mixin-class))))
-                             ))
-         (augm-class   (#+:LISPWORKS clos:ensure-class
-                        #+:SBCL      sb-mop:ensure-class
-                        class-name
-                        :direct-superclasses (list mixin-class obj-class)
-                        :metaclass           (class-of obj-class))
-                       ))
-    (change-class obj augm-class)))
-
-
+  (let* ((obj-class        (class-of obj))
+         (mixin-class      (if (symbolp mixin-class)
+                               (find-class mixin-class)
+                             mixin-class))
+         (mixin-class-name (class-name mixin-class))
+         (class-name       (cond ((null class-name)
+                                  (let ((obj-class-name (class-name obj-class)))
+                                    (intern (concatenate 'string
+                                                         (or (and name-prefix
+                                                                  (string name-prefix))
+                                                             (symbol-name mixin-class-name))
+                                                         "-"
+                                                         (package-name (symbol-package obj-class-name))
+                                                         "-"
+                                                         (symbol-name obj-class-name))
+                                            (symbol-package mixin-class-name))
+                                    ))
+                                 
+                                 ((symbolp class-name)
+                                  class-name)
+                                 
+                                 (t
+                                  (intern (string class-name)
+                                          (symbol-package mixin-class-name)))
+                                 ))
+         (augm-class       (#+:LISPWORKS clos:ensure-class
+                            #+:SBCL      sb-mop:ensure-class
+                            class-name
+                            :direct-superclasses (list mixin-class obj-class)
+                            :metaclass           (class-of obj-class))
+                           ))
+    (change-class obj augm-class)
+    ))
+    
