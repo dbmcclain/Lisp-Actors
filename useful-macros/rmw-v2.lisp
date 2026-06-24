94ce4277ba6aa1eca93e1cfcc8f93b28f3d58f31
David McClain <dbm@refined-audiometrics.com>
2026-06-24 04:07:11 -0700

    Update rmw-v2.lisp

--- useful-macros/rmw-v2.lisp
+++ useful-macros/rmw-v2.lisp
@@ -10,23 +10,92 @@
 (in-package #:com.ral.useful-macros.rmw-v2)
 
 ;; ----------------------------------------------------------
-;; To overcome the RMW ABA-Problem...
+;; ABA Problems and Common Lisp
 ;;
-;; We need to guard against ABA hazards during RMW. So we perform RMW
-;; with a 2-phase protocol: First acquire its value, while marking
-;; place as in-progress for update. The marking is done with a
-;; descriptor that enables any thread to help toward completion if we
-;; get interrupted. Then carry out the computation of the new value
-;; and perform the update, if it hasn't already been done by another
-;; thread on our behalf. This protocol requires two CAS operations.
+;; I contend that ABA problems cannot happen in Common Lisp because...
 ;;
-;; We can no longer simply read nor write the value in a shared
-;; location.  It might be in a state that is being updated by another
-;; thread. If so, we nudge it along to final resolution before
-;; acquiring/setting its value.
+;; In order to perform CAS on a pointer, you have to grab a copy of
+;; the pointer value for use as the old value in the CAS comparison
+;; operation.
+;;
+;; ABA problems occur when, after grabbing a copy, the item becomes
+;; deleted, a new and different object gets created using the same
+;; memory as the old item (possible due to MRU allocation), and then
+;; the pointer, with the same address, gets put back in place before
+;; we execute the CAS comparison. The CAS completes successfully and
+;; we go on thinking that our copy of the old item is the picture of
+;; what is actually there.
+;;
+;; The issue is that what looks like ABA is really ABC where Addr(C) =
+;; Addr(A). CAS sees ABA and produces ABX, where X is our locking
+;; placeholder value while we work with A. That CAS operation has just
+;; discarded C. Hence an ABA Problem.
+;;
+;; If we now modify A to become A′, and then use CAS to move from ABX
+;; to ABA′ and then exit, we will have updated the system based on
+;; stale information and discarded the true state of affairs.
+;;
+;; But this cannot happen in Common Lisp becaues the old item can
+;; never have been deleted and had its memory re-used for a different
+;; object. As long as there are extant references to data, that data
+;; memory will never be discarded by the GC and then reused for
+;; allocation.
+;;
+;; Object A can never become C before the CAS, but A could become A'
+;; where Addr(A') = Addr(A), e.g., the third element of a list could
+;; have been mutated without changing the address of the head of the
+;; list.  But we still have a reference to the list head as our old
+;; value, and any work we do on the list reflects its actual contents.
+;;
+;; So there can be no ABA problems in Lisp.
+;;
+----------------------------------------------------------
+;; The 2-phase CAS protocol -- used by lock-free algorithms to help
+;; speed along forward progress.
+;;
+;; If we succeed in CAS but our thread gets delayed while other
+;; threads get the CPU, the other threads will see a lock value in the
+;; CAS cell and be put-off from working until our original thread
+;; removes the lock.
+;;
+;; So in order to prevent this kind of stall, the lock value becomes
+;; an object that contains the old value prior to CAS, and a pointer
+;; to the update function. That update function should be idempotent
+;; and purely functional because it may be executed more than once.
+;;
+;; If the original thread gets delayed and another thread comes along
+;; to look at the pointer value, it will see the lock object and can
+;; help speed progress along by performing the update on behalf of the
+;; first thread, and then it can get the value it wants for itself.
+;;
+;; The twist here with RMW operations is that we often need more than
+;; one data value from the Read to properly perform a Modify/Write.
+;;
+;; For example, popping an item from the head of a list pointed to by
+;; a Ref cell: the RMW needs the CDR of the list for storage back into
+;; the Ref cell, while the user needs the value of the CAR cell that was
+;; popped off. So the update function used by RMW must supply 2
+;; values.
+;;
+;; That means that we can't just have another thread finish the update
+;; and stuff the new head address into the Ref cell for us. That
+;; wouldn't produce the CAR value we need for the original caller.
+;;
+;; So we resort to a secondary mechanism where any answers produced
+;; keeps a list of all values returned by the update function. The
+;; first item should always be what the unlocking CAS needs to see for
+;; the new value in the Ref Cell. And that is the value sought by any
+;; other reader threads. The remaining values pertain only to the
+;; original caller in the first thrad, and must be retained until the
+;; first thread runs again.
+;;
+;; But in following the 2-phase CAS protocol, we can no longer simply
+;; read nor write the value in a shared location.  It might be in a
+;; state that is being updated by another thread. If so, we nudge it
+;; along to final resolution before acquiring/setting its value.
 ;;
 ;; As long as all modifications to "place" are performed with WR or
-;; RMW then we can be assured that there will be no ABA hazards.
+;; RMW then we can be assured that the protocol is being followed.
 ;; Reading of place should be performed with RD, which helps push
 ;; along any update in progress before returning its stable value.
 ;;
