8475af78ed0e8bdf61a8dbcac66e9aae0448c436
David McClain <dbm@refined-audiometrics.com>
2026-07-20 08:24:16 -0700

    Update optima-bridge.lisp

--- xTActors/actors-base/optima-bridge.lisp
+++ xTActors/actors-base/optima-bridge.lisp
@@ -14,41 +14,72 @@
 
 ;; ----------------------------------
 
-(defun convert-pat (pat)        
-  (cond ((atom pat)
-         pat)
-        ((eq (car pat) 'quote)
-         pat)
-        (t
-         (let ((hd (convert-pat (car pat)))
-               (tl (convert-pat (cdr pat))))
-           (cond ((null tl)
-                  `(list ,hd))
-                 ((consp tl)
-                  `(,(car tl) ,hd ,@(cdr tl)))
-                 (t
-                  `(list* ,hd ,tl))
-                 )))
-        ))
+(defun convert-pat (pat &optional tlsyms tls)        
+  (if (or (atom pat)
+          (eq (car pat) 'quote))
+      (values pat tlsyms tls)
+    ;; else
+    (multiple-value-bind (hd tlsyms1 tls1)
+        (xconvert-pat (car pat) tlsyms tls)
+      (multiple-value-bind (tl tlsyms2 tls2)
+          (convert-pat (cdr pat) tlsyms1 tls1)
+        (cond ((null tl)
+               (values `(list ,hd)
+                       tlsyms2 tls2))
+              ((consp tl)
+               (values `(,(car tl) ,hd ,@(cdr tl))
+                       tlsyms2 tls2))
+              (t
+               (values `(list* ,hd ,tl)
+                       tlsyms2 tls2))
+              )))
+    ))
 
-(defun xconvert-pat (pat)
+(defun xconvert-pat (pat &optional tlsyms tls)
   ;; Convert lambda-list-keywords into a pattern that Optima can understand
+  ;;   (a b c &optional d) => (a b c . #:gd)
+  ;;   (a b c &rest d)     => (a b c . d)
   (let ((pos  (and (alexandria:proper-list-p pat)
                    (position-if (um:rcurry #'member lambda-list-keywords) pat))))
     (if pos
-        (let* ((hd  (um:take pos pat))
-               (tl  (um:drop pos pat))
-               (tlsym (gensym))
-               (new-pat (nconc hd tlsym)))
-          (values (convert-pat new-pat) tlsym tl))
-      (convert-pat pat))
+        (let ((hd  (um:take pos pat))
+              (tl  (um:drop pos pat)))
+          (optima:match tl
+            ((list* '&rest x y)
+             (convert-pat (nconc hd x)
+                          (if y
+                              (cons x tlsyms)
+                            tlsyms)
+                          (if y
+                              (cons y tls)
+                            tls)))
+            (_
+             (let ((tlsym   (gensym)))
+               (convert-pat (nconc hd tlsym)
+                            (cons tlsym tlsyms)
+                            (cons tl tls))
+               ))
+            ))
+      ;; else
+      (convert-pat pat tlsyms tls))
     ))
         
 #|
 (xconvert-pat 15)
 (xconvert-pat '(x 15 'x y))
 (xconvert-pat '(x 15 'x . y))
+(xconvert-pat '(x 15 'x &rest y))
+(xconvert-pat '(x 15 'x &rest y &key a b c))
+(xconvert-pat '((a &optional (b 1)) &rest args &key (x 15)))
  |#
+
+(defmacro bind-tail (args tail-var &body body)
+  `(apply (lambda ,args
+            ,@body)
+          ,tail-var))
+
+#+:LISPWORKS
+(editor:indent-like 'bind-tail 'multiple-value-bind)
 
 (defun translate-clause (clause)
   (um:let+ ((pat  (car clause))
@@ -56,18 +87,24 @@
             (:mvb (tst body) (if (member (car rest) '(when /))
                                  (values (cadr rest) (cddr rest))
                                (values nil rest)))
-            (:mvb (opat tlsym tl) (xconvert-pat pat))
-            (body (if tlsym
-                      `((bind-tail ,tl
-                            ,tlsym
-                          ,@body))
-                    body)))
-    (if tst
-        `(,opat (unless ,tst
-                  (optima:fail))
-                ,@body)
-      `(,opat
-        ,@body))
+            (body (if tst
+                      `((unless ,tst
+                          (optima:fail))
+                        ,@body)
+                    body))
+            (:mvb (opat tlsyms tls) (xconvert-pat pat))
+            (body (um:nlet iter ((body body)
+                                 (tlsyms tlsyms)
+                                 (tls    tls))
+                    (if (endp tlsyms)
+                        body
+                      ;; bindings to the right can refer to bindings on their left
+                      `((bind-tail ,(car tls)
+                            ,(car tlsyms)
+                           ,@(iter body (cdr tlsyms) (cdr tls))))
+                      ))))
+    `(,opat
+      ,@body)
     ))
 
 
@@ -93,14 +130,8 @@
 
 (translate-clause '((a b . c) / (eql c 15) (doit a b c)))
 (translate-clause '((a b &optional (c 32)) / (eql a 15) (doit a b c)))
+(translate-clause '(((a &optional (b 1)) &rest args &key (x 15)) / (eql a 15) (doit a b c)))
 (match msg
   ((a b . c) / (eql c 15) (doit a b c)))
  |#
 
-(defmacro bind-tail (args tail-var &body body)
-  `(apply (lambda ,args
-            ,@body)
-          ,tail-var))
-
-#+:LISPWORKS
-(editor:indent-like 'bind-tail 'multiple-value-bind)
